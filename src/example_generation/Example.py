e6ddd3f99f5498093e09ec92235ba4f3235fed4e
Kimyungi <gozj3319@naver.com>
2020-08-24 15:06:25 +0900

    Example : Eliminate generator_subtokenizer and Modfiy & Add some comment
    Signed-off-by: Kimyungi <gozj3319@naver.com>

--- src/example_generation/Example.py
+++ src/example_generation/Example.py
@@ -1,83 +1,23 @@
 from collections import defaultdict
-import tensorflow_datasets as tfds
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, Dense, LSTM, SimpleRNN, Bidirectional, GRU, Dropout, Masking
-import matplotlib.pyplot as plt
 from soynlp.tokenizer import LTokenizer
 from soynlp.word import WordExtractor
 import gensim
 import numpy as np
 
 # 불용어 처리하지 않고 sentence 추출
-def extract_sent(df, words):
+def extract_sent(df, words): # 전체 Dataset과 신어를 입력
     sent = defaultdict(lambda: 0)
     for w in words:
         temp = [s for s in df['head'] if w in s]
         sent[w] = '  '.join(temp)
-    return sent
-
-# Overfitting을 확인하기 위해 그래프 그리기
-def plot_history(histories, key='categorical_crossentropy'):
-    plt.figure(figsize=(16, 10))
-
-    for name, history in histories:
-        val = plt.plot(history.epoch, history.history['loss'],
-                       '--', label=name.title() + ' Val')
-        plt.plot(history.epoch, history.history['loss'], color=val[0].get_color(),
-                 label=name.title() + ' Train')
-
-    plt.xlabel('Epochs')
-    plt.ylabel(key.replace('_', ' ').title())
-    plt.legend()
-
-    plt.xlim([0, max(history.epoch)])
-
-class generator_subtokenizer:
-    # Training tokenizer
-    def __init__ (self, sent):
-        self.tokenizer = tfds.features.text.SubwordTextEncoder.build_from_corpus(sent, target_vocab_size=2 ** 13)
-
-    def create_var(self, sent):
-        sequences = list()
-        for line in sent:
-            encoded = self.tokenizer.encode(line)
-            temp = [encoded[:i+1] for i in range(1, len(encoded))]
-            sequences.extend(temp)
-
-        # padding 및 data split
-        self.max_len = max(len(l) for l in sequences)
-        sequences = pad_sequences(sequences, maxlen=self.max_len, padding='pre')
-        X = sequences[:, :-1]
-        y = sequences[:, -1]
-        y = to_categorical(y, num_classes=self.tokenizer.vocab_size)
-        return X, y
-
-    def model(self):
-        self.model = Sequential()
-        self.model.add(Embedding(self.tokenizer.vocab_size, 10, input_length= self.max_len - 1,
-                            mask_zero=True))  # y데이터를 분리하였으므로 이제 X데이터의 길이는 기존 데이터의 길이 - 1
-        self.model.add(GRU(64, return_sequences=True))
-        self.model.add(Dropout(0.4))
-        self.model.add(GRU(64, return_sequences=False))
-        self.model.add(Dropout(0.4))
-        self.model.add(Dense(self.tokenizer.vocab_size, activation='softmax'))
-        self.model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-    def sentence_generation(self, current_word, n):
-        for _ in range(n):  # n번 반복
-            encoded = self.tokenizer.encode(current_word)  # 현재 단어에 대한 정수 인코딩
-            encoded = pad_sequences([encoded], maxlen= self.max_len - 1, padding='pre')  # 데이터에 대한 패딩
-            result = self.model.predict_classes(encoded, verbose=0)
-            current_word = current_word + ' ' + self.tokenizer.decode([result[0]])  # 현재 단어 + ' ' + 예측 단어를 현재 단어로 변경
-        return current_word
-
-
+    return sent # Stopwords가 처리되지 않은, 신어가 포함된 모든 문장 출력
 
 class generator_ltokenizer:
-    # Training tokenizer
-    def __init__(self, words, sent):
+    # Training soynlp tokenizer
+    def __init__(self, words, sent): # 해당 신어와 그에 매치되는 sentences set 입력
         self.word_extractor = WordExtractor()
         self.words = words
         self.word_extractor.train(sent)  # 바꿀 것
@@ -86,19 +26,23 @@ class generator_ltokenizer:
         l_cohesions.update(self.words)
         self.tokenizer = LTokenizer(l_cohesions)
 
-    def train_word_model(self, sentences):
+    # 각 신어-문장 set에 대하여 Word2Vec Training
+    def train_word_model(self, sentences): # 위 학습된 tokenizer로 tokenizing된 sentences set 입력
         self.word_model = gensim.models.Word2Vec(sentences, size=100, min_count=1,
                                     window=2, iter=100)
         pretrained_weights = self.word_model.wv.syn0
         self.vocab_size, self.embedding_size = pretrained_weights.shape
 
+    # 입력된 단어에 대해 매치되는 index 반환
     def word2idx(self, word):
         return self.word_model.wv.vocab[word].index
 
+    # 입력된 index에 대해 매치되는 단어 반환
     def idx2word(self, idx):
         return self.word_model.wv.index2word[idx]
 
-    def create_var(self, sentences):
+    # 각 문장의 단어를 index로 변환
+    def create_var(self, sentences): # Tokenizing된 sentences set 입력
         for i in range(len(sentences)):
             for j in range(len(sentences[i])):
                 sentences[i][j] = self.word2idx(sentences[i][j])
@@ -112,8 +56,9 @@ class generator_ltokenizer:
         sequences = pad_sequences(sequences, maxlen=self.max_sentence_len, padding='pre')
         X = sequences[:, :-1]
         y = sequences[:, -1]
-        return X, y
+        return X, y # index set으로 변환하고 그것을 model에 fit 시킬 수 있도록 X변수와 y변수로 출력
 
+    # Bidirectional GRU Model 설계
     def model(self):
         self.model = Sequential()
         self.model.add(Embedding(self.vocab_size, self.embedding_size, input_length=self.max_sentence_len - 1))  # y데이터를 분리하였으므로 이제 X데이터의 길이는 기존 데이터의 길이 - 1
@@ -122,10 +67,11 @@ class generator_ltokenizer:
         self.model.add(Dense(self.vocab_size, activation='softmax'))
         self.model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-    def sentence_generation(self, word, n):
+    # 위 model을 이용하여 해당 신어에 대해 원하는 길이만큼의 문장 출력
+    def sentence_generation(self, word, n): # 신어와 원하는 길이를 입력
         word_idxs = [self.word2idx(word)]
         for _ in range(n):  # n번 반복
             encoded = pad_sequences([word_idxs], maxlen=self.max_sentence_len - 1, padding='pre')  # 데이터에 대한 패딩
             result = self.model.predict(encoded)
             word_idxs.append(np.argmax(result))
-        return ' '.join(self.idx2word(idx) for idx in word_idxs)
+        return ' '.join(self.idx2word(idx) for idx in word_idxs) # 생성된 문장 출력
