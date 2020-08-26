5410ce3d8d6e56ad4d9d9d8983bcf76e2a0e87bc
CJH <wtt5857@naver.com>
2020-08-26 21:46:35 +0900

    ADD : Modified Example.py

--- src/example_generation/Example.py
+++ src/example_generation/Example.py
@@ -13,6 +13,8 @@ def extract_sent(df, words): # 전체 Dataset과 신어를 입력
     for w in words:
         temp = [s for s in df['head'] if w in s]
         sent[w] = '  '.join(temp)
+        # DataFrame 에서 각 문장에서 신어가 포함되어있으면 그 문장을 temp에 저장한다. 
+        # sent dict에 key = 신어, value = 신어가 포함된 예문 전체(문장은 double space로 구분)로 저장한다. 
     return sent # Stopwords가 처리되지 않은, 신어가 포함된 모든 문장 출력
 
 class generator_ltokenizer:
@@ -21,15 +23,15 @@ class generator_ltokenizer:
         self.word_extractor = WordExtractor()
         self.words = words
         self.word_extractor.train(sent)  # 바꿀 것
-        cohesions = self.word_extractor.all_cohesion_scores()
-        l_cohesions = {word: score[0] for word, score in cohesions.items()}
+        cohesions = self.word_extractor.all_cohesion_scores()  # cohesion_scores를 cohesions에 저장
+        l_cohesions = {word: score[0] for word, score in cohesions.items()} # 각 단어와 각 단어의 cohesion_forward값을 l_cohesions에 저장
         l_cohesions.update(self.words)
-        self.tokenizer = LTokenizer(l_cohesions)
+        self.tokenizer = LTokenizer(l_cohesions) # LTokenizer 사용
 
     # 각 신어-문장 set에 대하여 Word2Vec Training
     def train_word_model(self, sentences): # 위 학습된 tokenizer로 tokenizing된 sentences set 입력
         self.word_model = gensim.models.Word2Vec(sentences, size=100, min_count=1,
-                                    window=2, iter=100)
+                                    window=2, iter=100) # Word2Vec Training
         pretrained_weights = self.word_model.wv.syn0
         self.vocab_size, self.embedding_size = pretrained_weights.shape
 
@@ -52,8 +54,8 @@ class generator_ltokenizer:
             for _ in range(1, len(line)):
                 sequence = line[:_ + 1]
                 sequences.append(sequence)
-        self.max_sentence_len = max([len(_) for _ in sentences])
-        sequences = pad_sequences(sequences, maxlen=self.max_sentence_len, padding='pre')
+        self.max_sentence_len = max([len(_) for _ in sentences]) # 전체 문장 중 최대 길이
+        sequences = pad_sequences(sequences, maxlen=self.max_sentence_len, padding='pre') # max_sentence_len을 기준으로 패딩
         X = sequences[:, :-1]
         y = sequences[:, -1]
         return X, y # index set으로 변환하고 그것을 model에 fit 시킬 수 있도록 X변수와 y변수로 출력
@@ -68,7 +70,7 @@ class generator_ltokenizer:
         self.model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
     # 위 model을 이용하여 해당 신어에 대해 원하는 길이만큼의 문장 출력
-    def sentence_generation(self, word, n): # 신어와 원하는 길이를 입력
+    def sentence_generation(self, word, n): # 신어와 원하는 길이를 입력하면 해당 길이 만큼 문장 생성
         word_idxs = [self.word2idx(word)]
         for _ in range(n):  # n번 반복
             encoded = pad_sequences([word_idxs], maxlen=self.max_sentence_len - 1, padding='pre')  # 데이터에 대한 패딩
