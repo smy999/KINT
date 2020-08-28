c6de5f736291398876fe1e41bbe3594d41c27c2c
smy999 <33407191+smy999@users.noreply.github.com>
2020-08-29 02:30:32 +0900

    KINT_v1.0.0: Update app.py
    comment, function def, global var, data rec

--- src/web/KINT_v1.0.0/app.py
+++ src/web/KINT_v1.0.0/app.py
@@ -6,186 +6,169 @@ from random import *
 import xlrd
 import math
 
+#######################################################################################
+
 app = Flask(__name__)
 
+################################## 전역 변수 #############################################3
 
-#################################################3
+# 각 함수에서 쓰이는 변수 값들 global 선언
 
-# Test Dataset
-
-# 1.예문
-# sentence = "예문 출력 Test입니다."
-
-# 2.감성분석 [0]=긍정, [1]=부정
-# sentimental = ["68%", "32%"]
-
-# 3.1. 분야(커뮤니티)별 비율
-
-# 3.1.1. x축 12달
-# dataset=[{
-#     "label": "Poitics",
-#     "data": [randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101)
-#              ]
-# },{
-#     "label": "Humor",
-#     "data": [randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101)
-#              ]
-# },{
-#     "label": "Entertainment",
-#     "data": [randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101)
-#              ]
-# },{
-#     "label": "News",
-#     "data": [randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101),
-#              randrange(1, 101)
-#              ]
-# }]
-
-# 3.1.2. x축 6개
-dataset={
-    "label": "Poitics",
-    "data": [randrange(1, 101),
-             randrange(1, 101),
-             randrange(1, 101),
-             randrange(1, 101),
-             randrange(1, 101),
-             randrange(1, 101),
-             randrange(1, 101),
-             randrange(1, 101),
-             randrange(1, 101),
-             randrange(1, 101),
-             randrange(1, 101),
-             randrange(1, 101)
-             ]
-}
-
-# 3.2. json 형식으로 변환
-jsdata = json.dumps(dataset, indent=4)
-print(jsdata)
-#4 이달의 신조어
-month_term = ["꿀잼", "상상코로나", "가성비", "가즈아", "나일리지"]
-
-
-#########################################################
-
-# 데이터셋 확인
-# print(sentence)
-# print(sentimental)
-# print(dataset)
-# print(jsdata)
-print(month_term)
-
-# labels = [_ for _ in dataset.keys()]
-# data = [_ for _ in dataset.values()]
-
-# for i in dataset:
-#     label.append([_ for _ in i.keys()])
-#     data.append([_ for _ in i.values()])
-
-###############################################################################3
-
-# 전역변수
-
-term=""
-
+# 입력 검색어
+term = ""
+# 신어 데이터에 검색어가 있는지 없는지 판단할 때 사용되는 flag
+varbreak = 0
+# 감성분석(긍정, 부정), 예문추출
+sens = list()
+# 연관 키워드
+rel_term = list()
+# 이달의 KINT
+month_term = list()
+# 날짜별 빈도 데이터
+dataDate = list()
+# 커뮤니티, 뉴스별 비율 데이터
+dataRef = list()
 
 
 ################################# 감성 분석 #############################################
 
-# 연관 키워드 엑셀 파일 불러오기
+# 감성 분석 엑셀 파일 불러오기
 sent = pd.read_excel('News_sentiment.xlsx')
 print(sent)
 
+# 신어 개수
 sentLen = len(sent)
 print(sentLen)
 
-# sentimental = list()
-#
-# term = "직구족"
-#
-# for i in range(sentLen):
-#     if term == sent['Word'][i]:
-#         print('yes')
-#         pos = sent['Positive'][i]
-#         print(type(pos))
-#         pos = math.trunc(pos)
-#         print(pos)
-#         neg = 100 - pos
-#         print(neg)
-#         sentimental.append(pos)
-#         sentimental.append(neg)
-#         break
-
+# 함수: 해당 검색어에 대한 정보 추출
+def sen(term):
+    global sens
+    sens = list()
+    for i in range(sentLen):
+        if term == sent['Word'][i]:
+            # 예문 가져오기
+            sens.append(sent['Sentence'][i])
+            # 감성 분석 결과 가져오기
+            pos = sent['Positive'][i]
+            pos = math.trunc(pos)
+            neg = 100 - pos
+            sens.append(str(pos)+"%")
+            sens.append(str(neg)+"%")
+            break
+    return sens
 
 ############################### 연관 키워드 #############################################
 
 # 연관 키워드 엑셀 파일 불러오기
-df2 = pd.read_excel('key.xlsx')
-# print(df2)
-
-# 신어 목록 출력
-# print(df2['key'])
+df2 = pd.read_excel('key_final.xlsx')
 
 # 신어 개수(= row 수)
 dfLen = len(df2)
 
 # 편의를 위해 row와 column 뒤집기
 df = df2.transpose()
-# print(df)
 
 # Nan 값 오류를 방지하기 위해 0으로 값 변경
 df = df.fillna(0)
-# print(df)
 
-# print(df.loc['key'])
+# 함수: 해당 검색어에 대한 정보 추출
+def rel(term):
+    global rel_term
+    global varbreak
+    rel_term = list()
+    varbreak = 0
+    for i in range(dfLen):
+        if term == df[i]['key']:
+            varbreak = 0
+            keyLen = len(df[i])
+
+            for j in range(0, keyLen - 1):
+                if df[i][j] == 0:
+                    continue
+                value = tuple(df[i][j].split("'"))
+                rel_term.append(value[1])
+            break
+        else:
+            varbreak = 1
+    return rel_term
+
+################################# 빈도 분석 #############################################
+
+# 빈도 엑셀 파일 불러오기
+newsDate = pd.read_excel('News_date.xlsx')
+print(newsDate)
+
+# 신어 개수
+dateNLen = len(newsDate)
+
+# NaN 값 0으로 변환
+newsDate = newsDate.fillna(0)
+
+# 날짜 순으로 정렬
+newsDate = newsDate.sort_index(axis=1)
+
+# 컬럼(커뮤니티, 신문사, key) 가져와서 key 빼고 저장: 그래프 labels에 쓰일 데이터
+labelDate = list(newsDate.columns)
+del labelDate[13]
+
+# 함수: 해당 검색어에 대한 정보 추출
+def date(term):
+    global dataDate
+    dataDate = list()
+    for i in range(dateNLen):
+        if term == newsDate['key'][i]:
+            dataDate = list(newsDate.loc[i])
+            del dataDate[13]
+            break
+    return dataDate
+
+################################# 이달의 KINT #############################################
+
+# 빈도 분석 파일에서 key와 마지막 달 column 가져와서 this_month에 저장
+this_month = newsDate[['key', labelDate[-1]]]
+print(this_month)
+
+# 마지막 달 컬럼 기준이로 내리차순 정렬 > top5 추출
+this_month = this_month.sort_values(by=[labelDate[-1]], ascending=[False])
+
+# 해당 검색어에 대한 정보 추출
+month_term = list(this_month['key'].head(5))
+
+################################# 비율 분석 #############################################
+
+# 비율 엑셀 파일 불러오기
+newsRef = pd.read_excel('News_ref.xlsx')
+
+# 신어 개수
+refNLen = len(newsRef)
+
+# NaN 값 0으로 변환
+newsRef = newsRef.fillna(0)
+
+# column(년월) 순 정렬
+newsRef = newsRef.sort_index(axis=1)
+
+# 한겨레 : 300, 경향신문 : 301, 매일경제 : 302, 조선일보 : 303, 디지털타임스 : 304, 동아일보 : 305, SBS뉴스 : 306, 한국경제 : 307
+labelRef = ["한겨레", "경향신문", "매일경제", "조선일보", "디지털타임스", "동아일보", "SBS뉴스", "한국경제"]
+
+# 함수: 해당 검색어에 대한 정보 추출
+def ref(term):
+    global dataRef
+    dataRef = list()
+    for i in range(refNLen):
+        if term == newsRef[0][i]:
+            for j in range(300, 307):
+                dataRef.append(newsRef[j][i]*100)
+            break
+    return dataRef
+
 ################################### 메인 페이지 ####################################################
 
 # home page
 @app.route('/')
 def home():
+    global month_term
+
     # 현재 실행 위치 출력
     print('home')
     # reauest 방식 확인 출력
@@ -209,6 +192,7 @@ def home():
 # search
 @app.route('/', methods=['POST', 'GET'])
 def search():
+    global month_term
     # 검색 기본 페이지 > search2.html
     render_template('search2.html', month_term=month_term)
     # 현재 실행 위치 출력
@@ -218,10 +202,6 @@ def search():
 
     # "POST" 일 때
     if request.method == 'POST':
-        # render_template('search2.html', month_term=month_term)
-
-        # 변수 초기화
-        result = ""
 
         # html에서 입력된 검색어 받아오기
         req = request.form.to_dict()
@@ -233,27 +213,7 @@ def search():
         print(term)
 
         # 검색어에 맞는 연관 키워드 추출
-        rel_term = list()
-        varbreak = 0
-        for i in range(dfLen):
-            if term == df[i]['key']:
-                varbreak = 0
-                print(df[i])
-                keyLen = len(df[i])
-                print(len(df[i]))
-                for j in range(0, keyLen - 1):
-                    if df[i][j] == 0:
-                        print(j,"is NaN")
-                        continue
-                    value = tuple(df[i][j].split("'"))
-                    print(value)
-                    rel_term.append(value[1])
-                break
-            else:
-                varbreak = 1
-        print(rel_term)
-
-
+        rel_term = rel(term)
 
         # 1. 입력된 검색어가 없을 때 처리방법
         if len(term) == 0:
@@ -261,111 +221,56 @@ def search():
             if 'mon' in req:
                 # 선택된 이달의 신조어를 검색어로 변경하고 필터링
                 term = req['mon']
-                print('mon이 있을 때')
+                print('선택된 이달의 신조어(mon)가 있을 때')
                 term = term.replace('#', '')
                 term = term.replace('들', '')
                 print(term)
 
                 # 바뀐 검색어에 대해 연관 키워드 추출
-                rel_term = list()
-                varbreak = 0
-                for i in range(dfLen):
-                    if term == df[i]['key']:
-                        varbreak = 0
-                        print(df[i])
-                        keyLen = len(df[i])
-                        print(len(df[i]))
-                        for j in range(0, keyLen - 1):
-                            if df[i][j] == 0:
-                                print(j, "is NaN")
-                                continue
-                            value = tuple(df[i][j].split("'"))
-                            print(value)
-                            rel_term.append(value[1])
-                        break
-                    else:
-                        varbreak = 1
-                print(rel_term)
+                rel_term = rel(term)
 
                 if varbreak == 1:
                     print('검색어에 맞는 결과 없음')
                     return render_template("search2.html", month_term=month_term, result="no", term=term)
                 else:
                     print('검색어에 맞는 결과 있음')
-
-                    sentimental = list()
-                    for i in range(sentLen):
-                        if term == sent['Word'][i]:
-                            # 예문 가져오기
-                            sentence = sent['Sentence'][i]
-                            # 감성 분석 결과 가져오기
-                            pos = sent['Positive'][i]
-                            pos = math.trunc(pos)
-                            neg = 100 - pos
-                            sentimental.append(str(pos)+"%")
-                            sentimental.append(str(neg)+"%")
-                            break
-
-                    return render_template('search.html', sent1=sentimental[0], sent2=sentimental[1], sentence=sentence,
-                                           term=term, dataset=dataset, jsdata=jsdata, rel_term=rel_term,
-                                           month_term=month_term)
-
-
+                    # 예문 출력, 감성 분석 결과 받아오기
+                    sens = sen(term)
+                    # 빈도 분석 결과 받아오기
+                    dataRef = ref(term)
+                    # 커뮤니티, 뉴스별 비율 받아오기
+                    dataDate = date(term)
+                    # search.html: 검색 결과 보여주기
+                    return render_template('search.html', sent1=(str(int(sens[1].replace('%', ''))+10)+'%'), sent2=(str(int(sens[2].replace('%', ''))+10)+'%'), sens=sens,
+                                           term=term, rel_term=rel_term, labelRef=labelRef, labelDate=labelDate,
+                                           dataDate=dataDate, dataRef=dataRef)
 
             # 1-2. 검색어도 없고, 이달의 신조어 클릭도 아닐 때 > 그냥 검색 버튼을 누르거나 검색창에서 Enter 눌렀을 때
             else:
-                print('mon이 없을 때')
-                # 검색어에 맞는 결과가 없을 때 > "검색 결과가 없습니다." 출력
+                print('선택된 이달의 신조어(mon)가 없을 때')
+                # search2.html: 아무것도 검색하지 않았을 때 > 검색 페이지 이동
                 return render_template("search2.html", month_term=month_term, result="")
+
 
         # 2. 입력된 검색어가 있을 때
         # 2-1. 위에서 필더링한 검색어의 결과가 없을 때 > 검색 페이지로 이동(search2.hmtl)
         if varbreak == 1:
-            print('검색 결과가 없습니다.')
+            print('입력된 검색어의 검색 결과가 없습니다.')
+            # search2.html: 검색어에 맞는 결과가 없을 때 > "검색 결과가 없습니다." 출력
             return render_template("search2.html", month_term=month_term, result="no", term=term)
+
         # 2-2. 위에서 필터링한 검색어의 결과가 있을 때 > 결과 페이지로 이동(search.html)
         else:
-            print('검색 결과가 있습니다.')
-
-            sentimental = list()
-            for i in range(sentLen):
-                if term == sent['Word'][i]:
-                    # 예문 가져오기
-                    sentence = sent['Sentence'][i]
-                    # 감성 분석 결과 가져오기
-                    pos = sent['Positive'][i]
-                    pos = math.trunc(pos)
-                    neg = 100 - pos
-                    sentimental.append(str(pos)+"%")
-                    sentimental.append(str(neg)+"%")
-                    break
-
-            return render_template('search.html', sent1=sentimental[0], sent2=sentimental[1], sentence=sentence,
-                                   term=term, dataset=dataset, jsdata=jsdata, rel_term=rel_term,
-                                   month_term=month_term)
-
-
-    # # "GET" 일 때
-    # if request.method == 'GET':
-    #     # term = request.args.get('term0')
-    #     req = request.form.to_dict()
-    #     print(req)
-    #
-    #     if len(req) == 0:
-    #         return render_template("search2.html")
-    #
-    #
-    #     term = req['term']
-    #
-    #     if len(term) == 0:
-    #         if 'mon' in req:
-    #             term = req['mon']
-    #         else:
-    #             return render_template("search2.html", month_term=month_term)
-    #
-    #     return render_template('search.html', sent1=sentimental[0], sent2=sentimental[1], sentence=sentence,
-    #                            term=term, dataset=dataset, jsdata=jsdata, rel_term=rel_term, month_term=month_term)
-
+            print('입력된 검색어의 검색 결과가 있습니다.')
+            # 예문 출력, 감성 분석 결과 받아오기
+            sens = sen(term)
+            # 빈도 분석 결과 받아오기
+            dataRef = ref(term)
+            # 커뮤니티, 뉴스별 비율 받아오기
+            dataDate = date(term)
+            # search.html: 검색 결과 보여주기
+            return render_template('search.html', sent1=sens[1], sent2=sens[2], sentence=sens[0],
+                                   term=term, rel_term=rel_term, labelRef=labelRef, labelDate = labelDate, dateDate = dataDate, dataRef = dataRef)
 
 
 
@@ -379,9 +284,6 @@ def search():
 # @app.errorhandler(404)
 # def page_not_found1(error):
 #     return redirect('/search2')
-
-
-
 
 
 ############################### 실행 #######################################################
